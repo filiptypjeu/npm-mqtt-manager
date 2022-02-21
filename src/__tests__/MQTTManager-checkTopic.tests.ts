11fc055e653a24e336236446abc9d77c7e8c0546
Filip Stenbacka <filip.stenbacka@gmail.com>
2022-02-21 23:32:53 +0200

    Simplified tests.

--- src/__tests__/MQTTManager-checkTopic.tests.ts
+++ src/__tests__/MQTTManager-checkTopic.tests.ts
@@ -1,109 +1,52 @@
 import { MQTTManager } from "../index";
 
 test("valid_subscriptions", () => {
-  var t = "test/abc";
-  expect(MQTTManager.checkTopic(t)).toEqual(true);
-
-  t = "test/another";
-  expect(MQTTManager.checkTopic(t)).toEqual(true);
+  expect(MQTTManager.checkTopic("test/abc")).toEqual(true);
+  expect(MQTTManager.checkTopic("test/another")).toEqual(true);
 });
 
 test("invalid_subscriptions", () => {
-  var t = "";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
-
-  t = "/";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
-
-  t = "test/abc/";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
-
-  t = "/test/abc";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
+  expect(MQTTManager.checkTopic("")).toEqual(false);
+  expect(MQTTManager.checkTopic("/")).toEqual(false);
+  expect(MQTTManager.checkTopic("test/abc/")).toEqual(false);
+  expect(MQTTManager.checkTopic("/test/abc")).toEqual(false);
 });
 
 test("valid_subscriptions_multilevel_wildcard", () => {
-  var t = "#";
-
-  expect(MQTTManager.checkTopic(t)).toEqual(true);
-
-  t = "test/#";
-  expect(MQTTManager.checkTopic(t)).toEqual(true);
-
-  t = "test/more/#";
-  expect(MQTTManager.checkTopic(t)).toEqual(true);
+  expect(MQTTManager.checkTopic("#")).toEqual(true);
+  expect(MQTTManager.checkTopic("test/#")).toEqual(true);
+  expect(MQTTManager.checkTopic("test/more/#")).toEqual(true);
 });
 
 test("invalid_subscriptions_multilevel_wildcard", () => {
-  var t = "test/#/hello";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
-
-  t = "test/#/#";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
-
-  t = "test/#/";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
-
-  t = "test/a#";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
-
-  t = "test/#a";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
-
-  t = "#a";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
-
-  t = "a#";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
+  expect(MQTTManager.checkTopic("test/#/hello")).toEqual(false);
+  expect(MQTTManager.checkTopic("test/#/#")).toEqual(false);
+  expect(MQTTManager.checkTopic("test/#/")).toEqual(false);
+  expect(MQTTManager.checkTopic("test/a#")).toEqual(false);
+  expect(MQTTManager.checkTopic("test/#a")).toEqual(false);
+  expect(MQTTManager.checkTopic("#a")).toEqual(false);
+  expect(MQTTManager.checkTopic("a#")).toEqual(false);
 });
 
 test("valid_subscriptions_singlelevel_wildcard", () => {
-  var t = "+";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
-
-  t = "test/+";
-  expect(MQTTManager.checkTopic(t)).toEqual(true);
-
-  t = "test/+/+";
-  expect(MQTTManager.checkTopic(t)).toEqual(true);
-
-  t = "test/+/more";
-  expect(MQTTManager.checkTopic(t)).toEqual(true);
-
-  t = "+/test";
-  expect(MQTTManager.checkTopic(t)).toEqual(true);
-
-  t = "test/+/extra/+/long/+/+";
-  expect(MQTTManager.checkTopic(t)).toEqual(true);
+  expect(MQTTManager.checkTopic("+")).toEqual(false);
+  expect(MQTTManager.checkTopic("test/+")).toEqual(true);
+  expect(MQTTManager.checkTopic("test/+/+")).toEqual(true);
+  expect(MQTTManager.checkTopic("test/+/more")).toEqual(true);
+  expect(MQTTManager.checkTopic("+/test")).toEqual(true);
+  expect(MQTTManager.checkTopic("st/+/extra/+/long/+/+")).toEqual(true);
 });
 
 test("invalid_subscriptions_singlelevel_wildcard", () => {
-  var t = "test/+no";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
-
-  t = "test/no+";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
-
-  t = "+no/";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
-
-  t = "test+/no+/hello";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
-
-  t = "test+/+no/hello";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
-
-  t = "++";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
-
-  t = "test/++";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
-
-  t = "test/+++/another";
-  expect(MQTTManager.checkTopic(t)).toEqual(false);
+  expect(MQTTManager.checkTopic("tets/+no")).toEqual(false);
+  expect(MQTTManager.checkTopic("test/no+")).toEqual(false);
+  expect(MQTTManager.checkTopic("+no")).toEqual(false);
+  expect(MQTTManager.checkTopic("test+/no")).toEqual(false);
+  expect(MQTTManager.checkTopic("++")).toEqual(false);
+  expect(MQTTManager.checkTopic("test/++")).toEqual(false);
+  expect(MQTTManager.checkTopic("test/+++/another")).toEqual(false);
 });
 
 test("valid_subscriptions_singlelevel_and_multilevel_wildcards", () => {
-  var t = "+/test/+/+/yes/+/#";
-  expect(MQTTManager.checkTopic(t)).toEqual(true);
+  expect(MQTTManager.checkTopic("+/test/+/+/yes/+/#")).toEqual(true);
 });
